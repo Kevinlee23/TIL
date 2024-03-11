4b4755dff7fbd099924315aafb2c9b814032f163
lixiuxian <15573097235@163.com>
2024-03-11 17:58:44 +0800

    fix: 设置默认输出地址

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/TIL/",
   title: "My TIL",
   description: "A VitePress Site",
   themeConfig: {
