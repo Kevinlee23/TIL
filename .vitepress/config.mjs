8bf8af0da2ed5fb230b8785467fa35e064c53495
lixiuxian <15573097235@163.com>
2024-03-11 17:46:41 +0800

    fix: 修改部署路径为默认配置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/TIL/",
   title: "My TIL",
   description: "A VitePress Site",
   themeConfig: {
